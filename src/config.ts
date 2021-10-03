5079777d79ab6df24a4d3e25201471ce4eed79e0
oky dwi hartanto <oky.hartanto@m2.formulatrix.com>
2021-10-03 10:51:03 +0700

    set default config to omdbapi

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const development = {
-  // SERVICE_URL: 'http://www.omdbapi.com/',
-  SERVICE_URL: 'http://localhost:11345',
+  SERVICE_URL: 'http://www.omdbapi.com/',
+  // SERVICE_URL: 'http://localhost:11345',
   API_KEY: 'faf7e5bb',
 };
 
